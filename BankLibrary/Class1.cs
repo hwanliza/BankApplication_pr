230591a4d38f926e63378a30b18139dff43ff843
hwanliza <90921120+hwanliza@users.noreply.github.com>
2021-09-18 12:34:21 +0700

    Добавьте файлы проекта.

---
+++ BankLibrary/Class1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+
+namespace BankLibrary
+{
+   
+    public interface IAccount
+    {
+        // Положить деньги на счет
+        void Put(decimal sum);
+        // Взять со счета
+        decimal Withdraw(decimal sum);
+    }
+
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        // Сообщение
+        public string Message { get; private set; }
+        // Сумма, на которую изменился счет
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+
+    }
+    public abstract class Account : IAccount
+    {
+        //Событие, возникающее при выводе денег
+        protected internal event AccountStateHandler Withdrawed;
+        // Событие возникающее при добавление на счет
+        protected internal event AccountStateHandler Added;
+        // Событие возникающее при открытии счета
+        protected internal event AccountStateHandler Opened;
+        // Событие возникающее при закрытии счета
+        protected internal event AccountStateHandler Closed;
+        // Событие возникающее при начислении процентов
+        protected internal event AccountStateHandler Calculated;
+
+        static int counter = 0;
+        protected int _days = 0; // время с момента открытия счета
+
+        public Account(decimal sum, int percentage)
+        {
+            Sum = sum;
+            Percentage = percentage;
+            Id = ++counter;
+        }
+
+        // Текущая сумма на счету
+        public decimal Sum { get; private set; }
+        // Процент начислений
+        public int Percentage { get; private set; }
+        // Уникальный идентификатор счета
+        public int Id { get; private set; }
+        // вызов событий
+        private void CallEvent(AccountEventArgs e, AccountStateHandler handler)
+        {
+            if (e != null)
+                handler?.Invoke(this, e);
+        }
+        // вызов отдельных событий. Для каждого события определяется свой витуальный метод
+        protected virtual void OnOpened(AccountEventArgs e)
+        {
+            CallEvent(e, Opened);
+        }
+        protected virtual void OnWithdrawed(AccountEventArgs e)
+        {
+            CallEvent(e, Withdrawed);
+        }
+        protected virtual void OnAdded(AccountEventArgs e)
+        {
+            CallEvent(e, Added);
+        }
+        protected virtual void OnClosed(AccountEventArgs e)
+        {
+            CallEvent(e, Closed);
+        }
+        protected virtual void OnCalculated(AccountEventArgs e)
+        {
+            CallEvent(e, Calculated);
+        }
+
+        public virtual void Put(decimal sum)
+        {
+            Sum += sum;
+            OnAdded(new AccountEventArgs("На счет поступило " + sum, sum));
+        }
+        // метод снятия со счета, возвращает сколько снято со счета
+        public virtual decimal Withdraw(decimal sum)
+        {
+            decimal result = 0;
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                result = sum;
+                OnWithdrawed(new AccountEventArgs($"Сумма {sum} снята со счета {Id}", sum));
+            }
+            else
+            {
+                OnWithdrawed(new AccountEventArgs($"Недостаточно денег на счете {Id}", 0));
+            }
+            return result;
+        }
+        // открытие счета
+        protected internal virtual void Open()
+        {
+            OnOpened(new AccountEventArgs($"Открыт новый счет! Id счета: {Id}", Sum));
+        }
+        // закрытие счета
+        protected internal virtual void Close()
+        {
+            OnClosed(new AccountEventArgs($"Счет {Id} закрыт.  Итоговая сумма: {Sum}", Sum));
+        }
+
+        protected internal void IncrementDays()
+        {
+            _days++;
+        }
+        // начисление процентов
+        protected internal virtual void Calculate()
+        {
+            decimal increment = Sum * Percentage / 100;
+            Sum = Sum + increment;
+            OnCalculated(new AccountEventArgs($"Начислены проценты в размере: {increment}", increment));
+        }
+    }
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый счет до востребования! Id счета: {this.Id}", this.Sum));
+        }
+    }
+    public class DepositAccount : Account
+    {
+        public DepositAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый депозитный счет! Id счета: {this.Id}", this.Sum));
+        }
+
+        public override void Put(decimal sum)
+        {
+            if (_days % 30 == 0)
+                base.Put(sum);
+            else
+                base.OnAdded(new AccountEventArgs("На счет можно положить только после 30-ти дневного периода", 0));
+        }
+
+        public override decimal Withdraw(decimal sum)
+        {
+            if (_days % 30 == 0)
+                return base.Withdraw(sum);
+            else
+                base.OnWithdrawed(new AccountEventArgs("Вывести средства можно только после 30-ти дневного периода", 0));
+            return 0;
+        }
+
+        protected internal override void Calculate()
+        {
+            if (_days % 30 == 0)
+                base.Calculate();
+        }
+    }
+    // тип счета
+    public enum AccountType
+    {
+        Ordinary,
+        Deposit
+    }
+    public class Bank<T> where T : Account
+    {
+        T[] accounts;
+
+        public string Name { get; private set; }
+
+        public Bank(string name)
+        {
+            this.Name = name;
+        }
+        // метод создания счета
+        public void Open(AccountType accountType, decimal sum,
+            AccountStateHandler addSumHandler, AccountStateHandler withdrawSumHandler,
+            AccountStateHandler calculationHandler, AccountStateHandler closeAccountHandler,
+            AccountStateHandler openAccountHandler)
+        {
+            T newAccount = null;
+
+            switch (accountType)
+            {
+                case AccountType.Ordinary:
+                    newAccount = new DemandAccount(sum, 1) as T;
+                    break;
+                case AccountType.Deposit:
+                    newAccount = new DepositAccount(sum, 40) as T;
+                    break;
+            }
+
+            if (newAccount == null)
+                throw new Exception("Ошибка создания счета");
+            // добавляем новый счет в массив счетов      
+            if (accounts == null)
+                accounts = new T[] { newAccount };
+            else
+            {
+                T[] tempAccounts = new T[accounts.Length + 1];
+                for (int i = 0; i < accounts.Length; i++)
+                    tempAccounts[i] = accounts[i];
+                tempAccounts[tempAccounts.Length - 1] = newAccount;
+                accounts = tempAccounts;
+            }
+            // установка обработчиков событий счета
+            newAccount.Added += addSumHandler;
+            newAccount.Withdrawed += withdrawSumHandler;
+            newAccount.Closed += closeAccountHandler;
+            newAccount.Opened += openAccountHandler;
+            newAccount.Calculated += calculationHandler;
+
+            newAccount.Open();
+        }
+        //добавление средств на счет
+        public void Put(decimal sum, int id)
+        {
+            T account = FindAccount(id);
+            if (account == null)
+                throw new Exception("Счет не найден");
+            account.Put(sum);
+        }
+        // вывод средств
+        public void Withdraw(decimal sum, int id)
+        {
+            T account = FindAccount(id);
+            if (account == null)
+                throw new Exception("Счет не найден");
+            account.Withdraw(sum);
+        }
+        // закрытие счета
+        public void Close(int id)
+        {
+            int index;
+            T account = FindAccount(id, out index);
+            if (account == null)
+                throw new Exception("Счет не найден");
+
+            account.Close();
+
+            if (accounts.Length <= 1)
+                accounts = null;
+            else
+            {
+                // уменьшаем массив счетов, удаляя из него закрытый счет
+                T[] tempAccounts = new T[accounts.Length - 1];
+                for (int i = 0, j = 0; i < accounts.Length; i++)
+                {
+                    if (i != index)
+                        tempAccounts[j++] = accounts[i];
+                }
+                accounts = tempAccounts;
+            }
+        }
+
+        // начисление процентов по счетам
+        public void CalculatePercentage()
+        {
+            if (accounts == null) // если массив не создан, выходим из метода
+                return;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                accounts[i].IncrementDays();
+                accounts[i].Calculate();
+            }
+        }
+
+        // поиск счета по id
+        public T FindAccount(int id)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i].Id == id)
+                    return accounts[i];
+            }
+            return null;
+        }
+        // перегруженная версия поиска счета
+        public T FindAccount(int id, out int index)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i].Id == id)
+                {
+                    index = i;
+                    return accounts[i];
+                }
+            }
+            index = -1;
+            return null;
+        }
+    }
+}
